b91d63ee7af43ccd76a0f8f442bdd539dcef051a
Quang-Nguyen <nvquang.it@gmail.com>
2023-10-29 20:45:51 +0700

    Fix FullScreen

--- schedule_details.py
+++ schedule_details.py
@@ -205,7 +205,7 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        MainWindow.showFullScreen()
+        MainWindow.showMaximized()
         MainWindow.setWindowFlag(Qt.FramelessWindowHint)
 
         self.retranslateUi(MainWindow)
